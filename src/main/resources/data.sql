4f14f7b3ecc4976a93311d2d6a397ce1cc8d1fde
Natan-333 <natancruz23@gmail.com>
2024-03-27 12:33:26 -0300

    atualizando Controller

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO ACTIVITY (title, description, score, status) 
-VALUES ('Teste', 'Criar os testes unitários', 10, 0);
+VALUES ('Teste', 'Criar os testes unitários', 10, 5);
 
 INSERT INTO ACTIVITY (title, description, score, status) 
-VALUES ('BD', 'Criar os diagrasmas do banco', 50, 15);
+VALUES ('BD', 'Criar os diagrasmas do banco', 50, 20);
 
 INSERT INTO ACTIVITY (title, description, score, status) 
-VALUES ('Figma', 'Prototipar as telas do app', 15, 25);
+VALUES ('Figma', 'Prototipar as telas do app', 15, 45);
