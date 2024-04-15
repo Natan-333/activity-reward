eeaaf3869ed6b23e0bd21e7efcb78215688c5ede
Natan-333 <natancruz23@gmail.com>
2024-04-15 16:41:17 -0300

    validações a consertar erro

--- src/main/java/br/com/fiap/activityrewards/activity/ActivityController.java
+++ src/main/java/br/com/fiap/activityrewards/activity/ActivityController.java
@@ -3,8 +3,11 @@ package br.com.fiap.activityrewards.activity;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.validation.Valid;
+
 @Controller
 @RequestMapping("activity")
 public class ActivityController {
 
     @Autowired
     ActivityRepository repository;
+
+    @Autowired
+    MessageSource messageSource;
 
     @GetMapping
     public String index(Model model) {
@@ -36,17 +44,19 @@ public class ActivityController {
         }
 
         repository.deleteById(id);
-        redirect.addFlashAttribute("message", "Tarefa apagada com sucesso");
+        redirect.addFlashAttribute("message", messageSource.getMessage("activity.delete", null, LocaleContextHolder.getLocale()));
         return "redirect:/activity";
     }
 
     @GetMapping("new")
-    public String form() {
+    public String form(Activity activity) {
         return "activity/form";
     }
 
     @PostMapping
-    public String create(Activity activity, RedirectAttributes redirect) {
+    public String create(@Valid Activity activity,BindingResult result, RedirectAttributes redirect) {
+        if(result.hasErrors()) return "activity/form";
+        
         repository.save(activity);
         redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso");
         return "redirect:/activity";
