7f2a904a9ac731f4f66b7aef1464c8a97e6563e5
Natan-333 <natancruz23@gmail.com>
2024-05-09 15:33:43 -0300

    Front-Service finalizado

--- src/main/java/br/com/fiap/activityrewards/activity/ActivityService.java
+++ src/main/java/br/com/fiap/activityrewards/activity/ActivityService.java
@@ -10,52 +10,6 @@ public class ActivityService {
   @Autowired
     ActivityRepository repository;
 
-     /**public void catchTask(Long id, User myuser) {
-        var activity = repository.findById(id).orElseThrow(
-            () -> new IllegalArgumentException("tarefa não encontrada")
-        );
-
-        if(activity.getUser() != null)
-            throw new IllegalArgumentException("tarefa já atribuída");
-
-        task.setUser(myuser);
-        repository.save(task);
-    }*/
-
-    /**public void dropActivity(Long id, User myuser) {
-        var activity = repository.findById(id).orElseThrow(
-            () -> new IllegalArgumentException("tarefa não encontrada")
-        );
-
-        if(activity.getUser() != myuser)
-            throw new IllegalArgumentException("tarefa atribuída a outro usuário");
-
-
-        activity.setUser(null);
-        repository.save(activity);
-    }
-
-    public void inc(Long id, User myuser) {
-        var activity = repository.findById(id).orElseThrow(
-            () -> new IllegalArgumentException("tarefa não encontrada")
-        );
-        if (activity.getStatus() +10 > 100) return;
-
-        activity.setStatus(activity.getStatus() + 10);
-        repository.save(activity);
-    }
-
-    public void dec(Long id, User myuser) {
-        var activity = repository.findById(id).orElseThrow(
-            () -> new IllegalArgumentException("tarefa não encontrada")
-        );
-
-        if (activity.getStatus() - 10 < 0) return;
-
-        activity.setStatus(activity.getStatus() - 10);
-        repository.save(activity);
-    }*/
-
     public void catchActivity(Long id, User myuser) {
         var activity = repository.findById(id).orElseThrow(
             () -> new IllegalArgumentException("tarefa não encontrada")
